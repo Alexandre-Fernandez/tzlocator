dd1a1435f257e692c3de2841d28c066b472ff2ff
alexandre-fernandez <alexandre-fernandez@gmx.com>
2022-08-15 19:34:40 +0200

    timezones & locators

--- src/classes/Tzlocator.ts
+++ src/classes/Tzlocator.ts
@@ -41,6 +41,24 @@ class Tzlocator {
 		return false
 	}
 
+	timezones() {
+		return Object.keys(timezones).filter(timezone =>
+			this.get(timezone, false)
+		) as Timezone[]
+	}
+
+	currencies() {}
+
+	languages() {}
+
+	locators() {
+		return Object.keys(timezones).reduce((prev, timezone) => {
+			const locator = this.get(timezone, false)
+			if (locator) prev.push(locator)
+			return prev
+		}, [] as Locator[])
+	}
+
 	private isValid(locator: Locator, useCache = true) {
 		if (this.validLocatorsCache[locator.code]) return true
 		if (this.isIncluded(locator) && !this.isExcluded(locator)) {
@@ -60,44 +78,6 @@ class Tzlocator {
 }
 
 // class Tzlocator {
-// 	private fallbackTimezoneData: TimezoneData | undefined
-
-// 	constructor(private timezonesMap: Timezones, fallback?: Timezone) {
-// 		if (fallback) this.fallbackTimezoneData = this.get(fallback)
-// 	}
-
-// 	has(timezone: LiteralUnion<Timezone>) {
-// 		return !!this.timezonesMap[timezone as Timezone]
-// 	}
-
-// 	/**
-// 	 * Returns the corresponding TimezoneData for the given Timezone, if the the
-// 	 * timezone does not exist it will return the fallback (set in the class
-// 	 * constructor), if a fallback was not given or does not exist, it will return
-// 	 * undefined.
-// 	 */
-// 	get(timezone: LiteralUnion<Timezone>): TimezoneData | undefined {
-// 		return (
-// 			this.timezonesMap[timezone as Timezone] || this.fallbackTimezoneData
-// 		)
-// 	}
-
-// 	/**
-// 	 * `predicate` will be called one for every available timezone, if it returns
-// 	 * `false` it will be filtered out, else it will be added to an object using
-// 	 * its `timezone` as a key.
-// 	 */
-// 	filter(predicate: (data: TimezoneData, timezone: Timezone) => boolean) {
-// 		return Object.entries(this.timezonesMap).reduce((prev, entry) => {
-// 			const [timezone, data] = entry as [Timezone, TimezoneData]
-// 			if (predicate(data, timezone)) prev[timezone] = data
-// 			return prev
-// 		}, {} as Partial<Timezones>)
-// 	}
-
-// 	fallback() {
-// 		return this.fallbackTimezoneData
-// 	}
 
 // 	timezones() {
 // 		return Object.keys(this.timezonesMap) as Timezone[]
